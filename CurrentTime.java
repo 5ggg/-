a2af3e08d65b6adebafcf735408863b6753836bb
JiaLiang Song (Leon) <56025832+5ggg@users.noreply.github.com>
2019-10-01 12:05:00 +0100

    Add files via upload

---
+++ CurrentTime.java
@@ -0,0 +1,12 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+ 
+public class CurrentTime{
+    public static void main(String[] args){
+        
+        SimpleDateFormat sdf = new SimpleDateFormat();// Clear Time
+        sdf.applyPattern("yyyy-MM-dd HH:mm:ss a");
+        Date date = new Date();// get current time
+        System.out.println("Current Time:   " + sdf.format(date)); 
+    } 
+}
